4c3d67cdd43428f50c9823a8f880f5c28a3e358c
LucaLmz <qumingquchangbuhuichong@outlook.com>
2025-06-20 19:31:52 +0800

    Add Class library

---
+++ TeamCode/src/main/java/pedroPathing/ClassLibrary/Claw.java
@@ -0,0 +1,40 @@
+package pedroPathing.ClassLibrary;
+
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Claw {
+    public enum Action{
+        Open,
+        Close
+    }
+    Servo Claw;
+    String Config;
+    boolean ClawFlag;
+    double OpenPosition;
+    double ClosePosition;
+    public void Act(Action ActionFlag){
+        if(ActionFlag==Action.Open){
+            Claw.setPosition(OpenPosition);
+            ClawFlag=true;
+        }
+        if(ActionFlag==Action.Close){
+            Claw.setPosition(ClosePosition);
+            ClawFlag=false;
+        }
+
+    }
+    public void Init(@NonNull HardwareMap hardwareMap){
+            Claw= hardwareMap.get(Servo.class,Config);
+    }
+    public void Switch(){
+        if(ClawFlag){
+            Act(Action.Close);
+        }else {
+            Act(Action.Open);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/pedroPathing/ClassLibrary/Rotate.java
@@ -0,0 +1,5 @@
+package pedroPathing.ClassLibrary;
+
+public class Rotate {
+
+}
