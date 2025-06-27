ebb2139f471471fed9590feb357ac5a8b8f3dd03
S65784 <16619896763@163.com>
2025-06-27 13:39:35 +0800

    Update LConstants.java

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -6,13 +6,13 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class LConstants {
     static {
-        TwoWheelConstants.forwardTicksToInches = .001989436789;
-        TwoWheelConstants.strafeTicksToInches = .001989436789;
-        TwoWheelConstants.forwardY = -4.83;
-        TwoWheelConstants.strafeX = 0.01;
-        TwoWheelConstants.forwardEncoder_HardwareMapName = "RightBehindMotor";//parallel
-        TwoWheelConstants.strafeEncoder_HardwareMapName = "LeftFrontMotor";//perpendicular
-        TwoWheelConstants.forwardEncoderDirection = Encoder.FORWARD;//FORWARD
+        TwoWheelConstants.forwardTicksToInches = .002;//.001989436789
+        TwoWheelConstants.strafeTicksToInches = .002;
+        TwoWheelConstants.forwardY = -4.83;//0.01
+        TwoWheelConstants.strafeX = 0.01;//-4.83
+        TwoWheelConstants.forwardEncoder_HardwareMapName = "LeftFrontMotor";//parallel RightBehindMotor
+        TwoWheelConstants.strafeEncoder_HardwareMapName = "RightBehindMotor";//perpendicular LeftFrontMotor
+        TwoWheelConstants.forwardEncoderDirection = Encoder.REVERSE;//FORWARD
         TwoWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
         TwoWheelConstants.IMU_HardwareMapName = "imu";
         TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
